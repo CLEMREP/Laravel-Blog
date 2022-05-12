068010f2078a2d2a91e8a9d293b5d1b3f6f0cd81
Clement <clement@web-id.fr>
2022-05-12 09:48:12 +0200

    Select Status Automatically

--- resources/views/partials/postform.blade.php
+++ resources/views/partials/postform.blade.php
@@ -41,8 +41,11 @@
                             <label class="text-xl text-gray-600">Status <span class="text-red-500">*</span></label></br>
                             <div class="inline-block relative w-64">
                                 <select name="published" class="block appearance-none w-full bg-white border-2 border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                                  <option value="1">Published</option>
-                                  <option value="0">Not Published</option>
+                                    <option value="0">Not Published</option>
+                                    <option value="1"
+                                    @if (old('published') || (!is_null($post) && $post->published))
+                                        selected
+                                    @endif>Published</option>
                                 </select>
                                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                   <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
